127e172e60abafea5d9cebf5939ab409d258eeb1
Seq73 <seq@seq.fi>
2015-10-27 13:58:27 +0200

    Update timesender.cpp

--- timesender.cpp
+++ timesender.cpp
@@ -9,22 +9,25 @@
 
 using namespace std;
 RF24 radio(RPI_V2_GPIO_P1_15, RPI_V2_GPIO_P1_24, BCM2835_SPI_SPEED_8MHZ);
-const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
+const uint64_t pipes[3]={0xF0F0F0F0E1LL,0xF0F0F0F0D2LL,0xF0F0F0F0F0LL};
 
 int main(int argc, char** argv){
   radio.begin();
   radio.enableDynamicPayloads();
   radio.setRetries(15,15);
-  radio.setPALevel( RF24_PA_MAX ) ;
+  radio.setDataRate(RF24_250KBPS);
+  radio.setPALevel(RF24_PA_MAX) ;
   radio.openWritingPipe(pipes[0]);
+  radio.openReadingPipe(1,pipes[1]);
+  radio.openReadingPipe(2,pipes[2]);
   radio.printDetails();
 
   float milliseconds;
   while (1){
     timeval curTime;
     gettimeofday(&curTime, NULL);
-    milliseconds = curTime.tv_usec / 1000;
-    if (milliseconds <= 1) break;
+    milliseconds=curTime.tv_usec/1000;
+    if(milliseconds<=1)break;
   }
 //printf("Millis: %f\n",milliseconds);
 
@@ -36,5 +39,5 @@ int main(int argc, char** argv){
   radio.stopListening();
 
   printf("Payload: %s size: %i",send_payload,sizeof(send_payload));
-  radio.write( send_payload, sizeof(send_payload) );
+  radio.write(send_payload,sizeof(send_payload));
 }
